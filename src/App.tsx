21d1b1e239b9f8698a0ba88ee082ff7d1d927c3e
Federico Turbino <f.turbino@liguriadigitale.it>
2026-07-27 16:26:22 +0200

    add test cases

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,39 @@ import { Button, Icon } from "design-react-kit";
 
 function App() {
   return (
-    <div style={{ padding: "2rem" }}>
+    <div
+      style={{
+        padding: "2rem",
+        display: "flex",
+        justifyContent: "center",
+        gap: "1rem",
+        flexDirection: "column",
+      }}
+    >
       <Button color="primary" icon>
         <Icon icon="it-star-full" color="white" aria-hidden />
-        <span>Conferma</span>
+        <span>No role</span>
+      </Button>
+      <Button color="primary" icon>
+        <Icon icon="it-star-full" color="white" aria-hidden role="none" />
+        <span>role="none"</span>
+      </Button>
+      <Button color="primary" icon>
+        <Icon
+          icon="it-star-full"
+          color="white"
+          aria-hidden
+          role="presentation"
+        />
+        <span>role="presentation"</span>
+      </Button>
+      <Button color="primary" icon>
+        <Icon icon="it-star-full" color="white" aria-hidden role="img" />
+        <span>role="img"</span>
+      </Button>
+      <Button color="primary" icon>
+        <Icon icon="it-star-full" color="white" aria-hidden role="asdfasdf" />
+        <span>role="asdfasdf"</span>
       </Button>
     </div>
   );
