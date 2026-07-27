4cb817ed5871abf0774185f254007e20303ef45f
Federico Turbino <f.turbino@liguriadigitale.it>
2026-07-27 14:58:07 +0200

    remove useless stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { Button, Icon } from 'design-react-kit'
+import { Button, Icon } from "design-react-kit";
 
 function App() {
   return (
-    <div style={{ padding: '2rem' }}>
-      <Button color='primary' icon>
-        <Icon icon='it-star-full' aria-hidden />
+    <div style={{ padding: "2rem" }}>
+      <Button color="primary" icon>
+        <Icon icon="it-star-full" color="white" aria-hidden />
         <span>Conferma</span>
       </Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap-italia/dist/css/bootstrap-italia.min.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
